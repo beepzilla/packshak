febc55156d043fe284d8a073f47e078e2c3dea53
beepzilla <beepzilla@example.com>
2024-02-12 09:34:46 -0500

    yarn

--- pages/myCards.tsx
+++ pages/myCards.tsx
@@ -1,7 +1,7 @@
-import { ThirdwebNftMedia, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import { useState } from "react";
+import { useAddress, useContract, useOwnedNFTs, ThirdwebNftMedia } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import { CARD_ADDRESS } from "../const/addresses";
-import { useState } from "react";
 import type { NFT as NFTType } from "@thirdweb-dev/sdk";
 import { ListingInfo } from "../components/ListingInfo";
 
@@ -10,14 +10,13 @@ export default function MyCards() {
 
     const {
         contract: nftCollection,
-        isLoading: loadingNFTCollection
+        isLoading: loadingNFTCollection,
     } = useContract(CARD_ADDRESS, "edition");
 
     const {
         data: nfts,
-        isLoading: loadingNFTs
+        isLoading: loadingNFTs,
     } = useOwnedNFTs(nftCollection, address);
-    console.log(nfts);
 
     const [selectedNFT, setSelectedNFT] = useState<NFTType>();
 
@@ -29,32 +28,43 @@ export default function MyCards() {
                     !loadingNFTCollection && !loadingNFTs ? (
                         nfts?.map((nft, index) => (
                             <div key={index} className={styles.nftCard}>
-                                <ThirdwebNftMedia
-                                    metadata={nft.metadata}
-                                />
+                                <ThirdwebNftMedia metadata={nft.metadata} />
                                 <div className={styles.myCardInfo}>
                                     <h3>{nft.metadata.name}</h3>
                                     <p>Qty: {nft.quantityOwned}</p>
+                                    {nft.metadata.description && (
+                                        <p className={styles.description}>{nft.metadata.description}</p>
+                                    )}
+                                    {nft.metadata.image && (
+                                        <img src={nft.metadata.image} alt={nft.metadata.name} style={{ width: '75%', height: 'auto' }} />
+                                    )}
+                                    {/* Displaying properties metadata */}
+                                    {nft.metadata.properties && (
+                                        <div className={styles.properties}>
+                                            <h4>Properties:</h4>
+                                            {Object.entries(nft.metadata.rarity).map(([key, value], i) => (
+                                                <p key={i}>{`${key}: ${value}`}</p>
+                                            ))}
+                                        </div>
+                                    )}
                                 </div>
                                 <button
                                     onClick={() => setSelectedNFT(nft)}
                                     className={styles.saleButton}
-                                >Sell Card</button>
+                                >
+                                    Sell Card
+                                </button>
                             </div>
                         ))
-                    ) :(
+                    ) : (
                         <p>Loading...</p>
                     )
                 ) : (
                     <div className={styles.saleCard}>
                         <div>
-                            <button
-                                onClick={() => setSelectedNFT(undefined)}
-                            >Back</button>
+                            <button onClick={() => setSelectedNFT(undefined)}>Back</button>
                             <br />
-                            <ThirdwebNftMedia
-                                metadata={selectedNFT.metadata}
-                            />  
+                            <ThirdwebNftMedia metadata={selectedNFT.metadata} />
                         </div>
                         <div>
                             <p>List card for sale</p>
@@ -64,5 +74,5 @@ export default function MyCards() {
                 )}
             </div>
         </div>
-    )
-};
+    );
+}
