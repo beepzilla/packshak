071c89ff573899251cde736106a564888ede7787
Josh <beepzilla@gmail.com>
2024-05-17 01:20:05 -0500

    Update PackNFT.tsx

--- components/PackNFT.tsx
+++ components/PackNFT.tsx
@@ -1,5 +1,5 @@
 import { MARKETPLACE_ADDRESS, PACK_ADDRESS } from "../const/addresses";
-import { Web3Button, useAddress, useContract, useDirectListings, useNFT, ThirdwebNftMedia } from "@thirdweb-dev/react";
+import { MediaRenderer, Web3Button, useAddress, useContract, useDirectListings, useNFT } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 type Props = {
@@ -14,7 +14,9 @@ export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
     const { contract: packContract } = useContract(contractAddress);
     const { data: packNFT, isLoading: loadingNFT } = useNFT(packContract, tokenId);
 
-    const { data: packListings, isLoading: loadingPackListings } = useDirectListings(
+
+    const { data: packListings, isLoading: loadingPackListings } = 
+    useDirectListings(
         marketplace,
         {
             tokenContract: PACK_ADDRESS,
@@ -29,19 +31,12 @@ export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
             txResult = await marketplace?.directListings.buyFromListing(
                 packListings[tokenId].id,
                 1
-            );
+            )
         } else {
             throw new Error("No valid listing found");
         }
-
+            
         return txResult;
-    };
-
-    const mediaStyle = {
-        maxHeight: '300px',
-        width: '100%',
-        objectFit: 'contain' as 'contain',
-        borderRadius: '8px'
     };
 
     return (
@@ -49,30 +44,24 @@ export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
             {!loadingNFT && !loadingPackListings ? (
                 <div className={styles.shopPack}>
                     <div>
-                        {packNFT?.metadata && (
-                            <ThirdwebNftMedia
-                                metadata={packNFT.metadata}
-                                style={mediaStyle}
-                            />
-                        )}
+                        <MediaRenderer
+                            src={packNFT?.metadata.image}
+                            width="80%"
+                            height="100%"
+                        />
                     </div>
                     <div className={styles.packInfo}>
                         <h3>{packNFT?.metadata.name}</h3>
-                        {packListings?.[tokenId] ? (
-                            <>
-                                <p>Cost: {packListings[tokenId].currencyValuePerToken.displayValue} {` ` + packListings[tokenId].currencyValuePerToken.symbol}</p>
-                                <p>Supply: {packListings[tokenId].quantity}</p>
-                                {!address ? (
-                                    <p>Login to buy</p>
-                                ) : (
-                                    <Web3Button
-                                        contractAddress={MARKETPLACE_ADDRESS}
-                                        action={() => buyPack()}
-                                    >Buy Pack</Web3Button>
-                                )}
-                            </>
+                        
+                        <p>Cost: {packListings![tokenId].currencyValuePerToken.displayValue} {` ` + packListings![tokenId].currencyValuePerToken.symbol}</p>
+                        <p>Supply: {packListings![tokenId].quantity}</p>
+                        {!address ? (
+                            <p>Login to buy</p>
                         ) : (
-                            <p>No active listings found</p>
+                            <Web3Button
+                            contractAddress={MARKETPLACE_ADDRESS}
+                            action={() => buyPack()}
+                            >Buy Pack</Web3Button>
                         )}
                     </div>
                 </div>
@@ -80,5 +69,5 @@ export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
                 <p>Loading...</p>
             )}
         </div>
-    );
+    )
 };
