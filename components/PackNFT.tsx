551c1717ca8da54868480937ed2a536754e7c6a9
Josh <beepzilla@gmail.com>
2024-05-17 04:36:08 -0500

    Update PackNFT.tsx

--- components/PackNFT.tsx
+++ components/PackNFT.tsx
@@ -2,12 +2,121 @@ import { MARKETPLACE_ADDRESS, PACK_ADDRESS } from "../const/addresses";
 import { ThirdwebNftMedia, Web3Button, useAddress, useContract, useDirectListings, useNFT } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 import styles from "../styles/Home.module.css";
-import { MarketplaceV3 } from "@thirdweb-dev/contracts";
 
 type Props = {
     contractAddress: string;
     tokenId: any;
 };
+
+// Define the MarketplaceV3 ABI
+const MarketplaceV3ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_listingId",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_quantity",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_totalPrice",
+                "type": "uint256"
+            }
+        ],
+        "name": "buyFromListing",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "DEFAULT_ADMIN_ROLE",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_extensionName",
+                "type": "string"
+            },
+            {
+                "internalType": "bytes4",
+                "name": "_functionSelector",
+                "type": "bytes4"
+            }
+        ],
+        "name": "_disableFunctionInExtension",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "contractType",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "contractURI",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "contractVersion",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_uri",
+                "type": "string"
+            }
+        ],
+        "name": "setContractURI",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
 
 export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
     const address = useAddress();
@@ -41,15 +150,19 @@ export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
                 // Create an ethers.js contract instance with the signer
                 const marketplaceWithSigner = new ethers.Contract(
                     MARKETPLACE_ADDRESS,
-                    MarketplaceV3.abi,
+                    MarketplaceV3ABI,
                     signer
                 );
+
+                // Calculate the total price based on the listing details
+                const totalPrice = packListings[tokenId].currencyValuePerToken.value.mul(1);
 
                 // Call the buyFromListing method with the specified gas limit
                 const tx = await marketplaceWithSigner.buyFromListing(
                     packListings[tokenId].id,
                     1,
-                    { gasLimit: 2000000 }
+                    totalPrice,
+                    { gasLimit: 2000000, value: totalPrice }
                 );
                 txResult = await tx.wait();
             } catch (error) {
